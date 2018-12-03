aa9583dcb1ba6cfa88c17ad62a95defc45f5c2b0
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-04 00:34:32 +0100

    Add buttons explorer

---
+++ packages/phoenix-buttons/explorer/README.md
@@ -0,0 +1,17 @@
+# Phoenix Buttons Explorer
+
+## Features
+
+* phoenix-buttons
+
+* React 16
+* Webpack 4
+* Babel 7
+* ES6
+* Hot Module Replacement
+
+## Installation
+
+* npm install
+* npm start
+* visit `http://localhost:7007/`
---
+++ packages/phoenix-buttons/explorer/webpack.config.js
@@ -0,0 +1,100 @@
+/* eslint global-require : 0 */
+
+const autoprefixer = require('autoprefixer')
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              // modules: true,
+              sourceMap: true,
+              localIdentName: '[local]__[hash:base64:5]',
+              minimize: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              sourceMap: true,
+              plugins: () => [
+                autoprefixer({
+                  browsers: ['>1%', 'last 4 versions', 'Firefox ESR', 'not ie < 9'],
+                }),
+              ],
+            },
+          },
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: require.resolve('postcss-loader'),
+            options: {
+              ident: 'postcss',
+              plugins: () => [
+                require('postcss-flexbugs-fixes'),
+                autoprefixer({
+                  browsers: [
+                    '>1%',
+                    'last 4 versions',
+                    'Firefox ESR',
+                    'not ie < 9',
+                  ],
+                  flexbox: 'no-2009',
+                }),
+              ],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(ttf|eot|woff(2)?)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    port: 7001,
+    contentBase: './dist',
+    hot: true,
+  },
+};
