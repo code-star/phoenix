c1fbc57a046f5b4e73a85bb06a964e8b62dc5d63
Mehmet Tuğrul Şahin <mehmettugrulsahin@gmail.com>
2018-12-04 01:16:14 +0100

    Remove storybook webpack configuration

--- .storybook/webpack.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = (baseConfig, env, defaultConfig) => {
-  defaultConfig.module.rules.push({
-    test: /\.(js|jsx)$/,
-    loader: require.resolve('babel-loader'),
-  });
-  defaultConfig.resolve.extensions.push('.js', '.jsx');
-  return defaultConfig;
-};
